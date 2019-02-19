558ad5922961fb95c9a754a0eca10c22904d4732
Christopher Tracey <ctracey@paconserve.org>
2019-02-19 16:19:35 -0500

    some minor changes to get it to work new model
    needs more long term work

--- SDM_VariableImportance.r
+++ SDM_VariableImportance.r
@@ -5,16 +5,17 @@
 #bottom that can/should be removed from the analyses?
 
 # originally coded by Tim Howard at some point in the past, heavily modified by Christopher Tracey in 2018
+# 2019-02-19 - modify to current modeling code
 
 library(RSQLite)
 library(randomForest)
 
 # First, change to the directory
-setwd("E:/SDM/Aquatic2/outputs") # set to the SDM output directory
+setwd("E:/SDM/Shared/MostImportantVariables/mussels") # set to the SDM output directory
 # Second, define an empty SQLlite db
-databasename <- "E:/SDM/Shared/MostImportantVariables/MostImportantVar_Aquatic.sqlite" # this should be an empty sqlite database, script will create and populate the table
+databasename <- "E:/SDM/Shared/MostImportantVariables/MostImportantVar_Mussels.sqlite" # this should be an empty sqlite database, script will create and populate the table
 # Third, set an output name for png file for the boxplots, this will be saved in the working directory
-boxfile <- "PA_aquatic" # .png will be appended by script
+boxfile <- "PA_mussels" # .png will be appended by script
 
 #get a list of what's in the directory
 d <- dir(pattern="Rdata", recursive=TRUE, include.dirs=TRUE)  # modified to deal with storing Rdata in species specific subfolders
@@ -54,7 +55,6 @@ for (i in 1:length(d)){
 #get data and create box plots for variable importance across a set of models
 db <- dbConnect(SQLite(), dbname = databasename)
 #write importance values
-###importance <- sqlQuery(channel = Cn.MDB.out, query = "SELECT * FROM tblImportance")
 SQLquery_imp <- paste("SELECT * FROM tblImportance")
 importance  <- dbGetQuery(db, statement=SQLquery_imp )
 dbDisconnect(db) #close connection
